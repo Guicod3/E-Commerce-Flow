039c4ca6929d2a8e22b8e03e94872db62b0f143d
Guilherme Miranda <guilmiranda50@gmail.com>
2024-08-31 18:54:28 -0300

    feat: style done

--- src/public/logic/formatText.js
+++ src/public/logic/formatText.js
@@ -3,13 +3,15 @@ function textFormat() {
     const textBeforeFormat = getinput.split('\n');
     let textAfterFormat = '';
     const output = document.getElementById('output');
+    const outputHTML = document.getElementById('outputHTML')
 
     const topicsSubtitle = 'acompanha:'
     const importante = '<br><p style="font-weight: bold">Importante:</p><p>A instalação deve ser feita por um profissional especializado.</p><p>Nós não nos responsabilizamos por instalação mal realizada.</p><p>As imagens são meramente ilustradas, pode ocorrer mudança sem prévio aviso, porém as características técnicas são sempre preservadas.</p>';
 
     if (getinput === ''){
-        textAfterFormat += '<p>O Campo de entrada vazio. Por favor, insira um valor.</p>';
+        textAfterFormat += '<p>O Campo de entrada está vazio. Por favor, insira um valor.</p>';
         output.innerHTML = textAfterFormat;
+        outputHTML.innerHTML = textAfterFormat;
     } else{
         for (let index = 0; index < textBeforeFormat.length; index++){
             const useLine = textBeforeFormat[index].trim()
@@ -19,13 +21,13 @@ function textFormat() {
             // Verifica se a linha é um título
             if (useLine === ''){
                     const useindex = index + 1
-                    textAfterFormat += `<br> <h2 style="font-size: 14pt">${textBeforeFormat[useindex]}</h2>`;
+                    textAfterFormat += `<br> <strong><h2 style="font-size: 14pt">${textBeforeFormat[useindex]}</h2></strong>`;
                     index = useindex
             }
     
             // Verifica se é a primeira linha
             else if (index === 0) { 
-                textAfterFormat += `<h1 style="font-size: 14pt">${textBeforeFormat[index]}</h1>`;
+                textAfterFormat += `<strong><h1 style="font-size: 14pt">${textBeforeFormat[index]}</h1></strong>`;
             } 
     
             // Verifica se é um subtítulo
@@ -38,9 +40,12 @@ function textFormat() {
                 textAfterFormat += `<p style="font-size: 12pt">- ${textBeforeFormat[index]}</p>`;
             }
         };
-    
+        
         textAfterFormat += importante;
+        output.className = 'w-full shadow-md rounded-xl h-auto bg-white mt-2 p-3 mb-10'
+        outputHTML.className = 'w-full shadow-md rounded-xl h-auto bg-white mt-2 p-3 mb-10'
         output.innerHTML = textAfterFormat;
+        outputHTML.textContent = textAfterFormat;
     }
 }
 
