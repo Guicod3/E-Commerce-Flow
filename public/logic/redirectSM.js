6a76b871472f3677eb383575095ac846dd739ff6
Guilherme Miranda <guilmiranda50@gmail.com>
2024-09-22 18:47:08 -0300

    feat: add solve bug

---
+++ public/logic/redirectSM.js
@@ -0,0 +1,7 @@
+document.getElementById('DescriçãoSM').addEventListener('click', () =>{
+    window.location.assign(`/description`)
+})
+ 
+document.getElementById('AnunciosSM').addEventListener('click', () =>{
+    window.location.assign(`/ads`)
+})
