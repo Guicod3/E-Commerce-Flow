d9154a1857886a8463ef1a32d5daa0f0a4093f73
Guilherme Miranda <guilmiranda50@gmail.com>
2024-09-26 23:33:30 -0300

    feat: update ai

--- src/aiLogicModel/AIResponseAnuncios.js
+++ src/aiLogicModel/AIResponseAnuncios.js
@@ -1,78 +1,49 @@
 import dotenv from 'dotenv';
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { TextLoader } from "langchain/document_loaders/fs/text";
 import { z } from 'zod'
 import { StructuredOutputParser } from 'langchain/output_parsers';
 dotenv.config()
 
-export async function getTitlesAnunciosAI(content, numberTitles) {
+export async function getTitlesAnunciosAI(content, numberTitles, type) {
+  let loader;
+  if (type === "titulo") {
+    loader = new TextLoader("src/aiLogicModel/tools/createTitle.txt");
+  } else if (type === "descricao") {
+    loader = new TextLoader("src/aiLogicModel/tools/createDesc.txt");
+  }
+
+  const docs = await loader.load()
 
   const model = new ChatGoogleGenerativeAI({
     model: "gemini-1.5-flash",
-    temperature: 1,
+    temperature: 0.7,
     maxRetries: 2,
     apiKey: process.env.GOOGLE_API_KEY,
   });
-
-  // Configurando o template de prompt corretamente
-  const template = ChatPromptTemplate.fromTemplate(`
-Persona: Marketing and SEO Specialist who makes titles using as many words as possible to get close or pass a little to the 60-character limit and that for questions or inputs outside of the context you always use the response for invalid content 'Sou um agente criador de títulos, não posso te ajudar com isso.'
-
-Objective: Create optimized ad titles for products on marketplaces, adhering to SEO and marketing rules.
-
-Task:
-Generate titles for the provided content, in the requested quantity.
-These titles will be used to promote products on marketplace platforms.
-
-Guidelines:
-Title Structure (order doesn't need to be followed):
-Type of product (e.g., Kit, Pair, Speaker, Grill, etc.)
-Brand
-Model
-Key features (e.g., power, size, non-stick, etc.)
-Voltage (if applicable)
-
-<important>Select the product features and insert them according to the following pattern:
-Type of product brand model feature 1 feature 2 feature 3... You can mix these structures to create different titles, sometimes adding different features, sometimes omitting the model name, or something similar.
-Examples:
-- Type of product brand model feature 1 feature 2 feature 3
-- brand model feature 1 feature 2 feature 3 Type of product
-- model brand feature 1 feature 2 Type of product feature 3
-- brand Type of product feature 1 model feature 2 feature 3
-</important>
-
-Formatting Requirements:
-Maximum of 60 characters.
-Avoid special characters, such as commas, hyphens, or symbols.
-Different titles for each suggestion (avoid unnecessary word repetition).
-
-Invalid Content:
-Text without a connection to products.
-Text with incorrectly formed words.
-Text containing offensive or inappropriate language.
-Important Note:
-
-Ensure each title is unique and follows the proposed structure.
-Provided Content: {content}
-Number of Titles: {numberTitles}
-Formatting Instructions: {format_instructions}
-Response for Invalid Content: {resposta}
-
-Language for the response: Portuguese
-`);
 
     const outputParser = StructuredOutputParser.fromZodSchema(
       z.object({
         titles: z.array(z.string()).describe('Títulos')
       })
     )
-  const chain = template.pipe(model).pipe(outputParser);
+  //const chain = loader.pipe(model).pipe(outputParser);
 
-  return await chain.invoke({
+  return await model.invoke({
+    context: docs,
     content: content,
     numberTitles: numberTitles,
     format_instructions: outputParser.getFormatInstructions(),
-    resposta: 'Sou um agente criador de títulos, não posso te ajudar com isso.'
   });
 }
 
+const teste = await getTitlesAnunciosAI(`Grill Elétrica Elgin Break Time 800W 2 Em 1 Antiaderente Luz Indicadora Trava Fechamento
+Grill Duplo Antiaderente
+Duplo sistema de aquecimento, 1 para cada placa
+Revestimento antiaderente
+Trava de fechamento
+Superfície tocável
+Base antiderrapante
+Proteção contra superaquecimento`, 3, 'titulo')
+
+console.log(test)
